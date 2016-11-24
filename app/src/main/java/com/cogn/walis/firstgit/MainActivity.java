ba67922dd2c77b0c70ff3dda762833f9bfe7fa79
yyddiablo <1276120630@qq.com>
2016-11-24 19:55:24 +0800

    2016-11-24 19:54:59

--- app/src/main/java/com/cogn/walis/firstgit/MainActivity.java
+++ app/src/main/java/com/cogn/walis/firstgit/MainActivity.java
@@ -1,13 +1,23 @@
 package com.cogn.walis.firstgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.main_tv1)
+    TextView mainTv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mainTv1.setText("'com.jakewharton:butterknife:7.0.1'");
     }
 }
